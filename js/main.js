1beb4f3d4c3febd0c90bb2b31f83936876517c7c
acroak <acroak@live.com>
2018-05-17 11:21:25 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo ="king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardsInPlay = [];
 
-console.log ("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+// Variable to represent the first card the user flipped
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log ("user flipped queen");
+
+//Variable to represent the 2nd card flipped
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped king");
+
+//add logic to check to see if two cards have been played
+if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ){
+	alert("You found a match!");
+}else{
+	alert("Sorry, try again.");
+}
