1285941323b479ce8dfdec1dafd946185dc505d2
acroak <acroak@live.com>
2018-05-18 21:44:51 -0400

    created game logic unit 9

--- js/main.js
+++ js/main.js
@@ -2,21 +2,27 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-// Variable to represent the first card the user flipped
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log ("user flipped queen");
-
-//Variable to represent the 2nd card flipped
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped king");
-
-//add logic to check to see if two cards have been played
-if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ){
-	alert("You found a match!");
-}else{
-	alert("Sorry, try again.");
+var checkForMatch = function(){
+	if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.");
+	}
 }
+
+
+var flipCard = function(cardId){
+
+	checkForMatch();
+
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	};
+
+
+
+flipCard(0);
+flipCard(2);
